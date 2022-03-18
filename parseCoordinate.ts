b0f445d3aaa66e2196849d43bb0249da9aac1efe
Mowalky <dev@mowalky.com>
2022-03-17 22:07:48 -0500

    updated

--- parseCoordinate.ts
+++ parseCoordinate.ts
@@ -25,5 +25,20 @@ function parseCoordinate(arg1:unknown,arg2?:unknown):Coordinate {
         x: 0,
         y: 0,
     }
+
+    if (typeof arg1 === 'object') {
+        coord = {
+            ...(arg1 as Coordinate),
+        }
+    } else {
+        coord = {
+            x: arg1 as number,
+            y: arg2 as number,
+        }
+    }
+    
     return coord;
 }
+
+console.log(parseCoordinate(10,20));
+console.log(parseCoordinate({x:43,y:40}));
